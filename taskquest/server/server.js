a20b1c4573365ee500dd7d86008fe6321a597111
cy22chi08 <jerichocyrus2208@gmail.com>
2026-03-30 22:03:56 +0800

    wa

--- taskquest/server/server.js
+++ taskquest/server/server.js
@@ -8,7 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static("public"));
+const path = require("path");
+app.use(express.static(path.join(__dirname, "../public")));
 
 
 /* =========================
